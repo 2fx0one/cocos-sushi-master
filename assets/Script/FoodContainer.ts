3dc31fde652cbbd759662615cef109d1ed013618
2fx0one <2fx0one@gmail.com>
2019-12-02 23:43:02 +0800

    update

--- assets/Script/FoodContainer.ts
+++ assets/Script/FoodContainer.ts
@@ -21,22 +21,23 @@ export default class FoodContainer extends cc.Component {
 
     init() {
         let data = [
-            { x: 450, y: 250, foodName: "13", foodId: "13", picPath: '13', amount: 10 },
-            { x: 350, y: 250, foodName: "zushi", foodId: "1", picPath: '1', amount: 10 },
-            { x: 350, y: 150, foodName: "2", foodId: '2', picPath: '2', amount: 10 },
-            { x: 350, y: 50, foodName: "3", foodId: '3', picPath: '3', amount: 10 },
+            { x: 450, y: 250, foodName: "虾", foodId: "13", picPath: '13', amount: 10 },
 
-            { x: 250, y: 250, foodName: "1", foodId: '4', picPath: '4', amount: 10 },
-            { x: 250, y: 150, foodName: "2", foodId: '5', picPath: '5', amount: 10 },
-            { x: 250, y: 50, foodName: "3", foodId: '6', picPath: '6', amount: 10 },
+            { x: 350, y: 250, foodName: "饭", foodId: '1', picPath: '1', amount: 10 },
+            { x: 350, y: 150, foodName: "海苔", foodId: '2', picPath: '2', amount: 10 },
+            { x: 350, y: 50, foodName: "鲑鱼子", foodId: '3', picPath: '3', amount: 10 },
 
-            { x: 150, y: 250, foodName: "4", foodId: '7', picPath: '7', amount: 10 },
-            { x: 150, y: 150, foodName: "5", foodId: '8', picPath: '8', amount: 10 },
-            { x: 150, y: 50, foodName: "6", foodId: '9', picPath: '9', amount: 10 },
+            { x: 250, y: 250, foodName: "鲑鱼", foodId: '4', picPath: '4', amount: 10 },
+            { x: 250, y: 150, foodName: "5", foodId: '5', picPath: '5', amount: 10 },
+            { x: 250, y: 50, foodName: "黄瓜", foodId: '6', picPath: '6', amount: 10 },
 
-            { x: 50, y: 250, foodName: "7", foodId: '1', picPath: '10', amount: 10 },
-            { x: 50, y: 150, foodName: "8", foodId: '1', picPath: '11', amount: 10 },
-            { x: 50, y: 50, foodName: "9", foodId: '1', picPath: '12', amount: 10 }
+            { x: 150, y: 250, foodName: "扁口鱼", foodId: '7', picPath: '7', amount: 10 },
+            { x: 150, y: 150, foodName: "8", foodId: '8', picPath: '8', amount: 10 },
+            { x: 150, y: 50, foodName: "章鱼", foodId: '9', picPath: '9', amount: 10 },
+
+            { x: 50, y: 250, foodName: "10", foodId: '10', picPath: '10', amount: 10 },
+            { x: 50, y: 150, foodName: "11", foodId: '11', picPath: '11', amount: 10 },
+            { x: 50, y: 50, foodName: "12", foodId: '12', picPath: '12', amount: 10 }
         ]
         data.forEach((v, i) => {
             this.foodsInContainMap[v.foodId] = this.createFood(v.x, v.y, v.foodId, v.foodName, v.picPath, v.amount)
--- assets/Script/SushiMenu.ts
+++ assets/Script/SushiMenu.ts
@@ -10,13 +10,13 @@ export default class SushiMenu extends cc.Component {
     private recipes: { [key: string]: Recipe } = {}
 
 
-    onLoad () {
+    onLoad() {
         Singleton.Instance.sushiMenu = this
         this.init()
     }
 
     init() {
-                // {
+        // {
         //     '1_1_2': ['01', '01', '01'],
         //     '1_3': ['02', '02', '02'],
         //     '1_2_3_3': ['03', '03', '03'],
@@ -29,8 +29,20 @@ export default class SushiMenu extends cc.Component {
         //寿司菜单
         let sushiData = [
             // { sushiName: 'basic', picPath: '01', inputFoodId: ['1', '1', '2'], outputSushiId: ['01', '01', '01'] }
-            new Recipe('饭+菜', '01', ['1', '1', '2'], ['01', '01', '01']),
-            new Recipe('饭+鲑鱼子', '02', ['1', '3'], ['02', '02', '02']),
+            new Recipe('饭团 2饭+1海苔', '01', ['1', '1', '2'], ['01', '01', '01']),
+            new Recipe('军舰 1饭+1鲑鱼子', '02', ['1', '3'], ['02', '02', '02']),
+            new Recipe('军舰 1饭+2海苔+1鲑鱼子', '03', ['1', '2', '2', '3'], ['03', '03', '03']),
+            new Recipe('寿司 2饭+1海苔+1鲑鱼', '04', ['1', '1', '2', '4'], ['04', '04', '04']),
+            new Recipe('寿司 1饭+2海苔+1黄瓜', '05', ['1', '2', '2', '6'], ['05', '05', '05']),
+            new Recipe('寿司 1饭+2海苔+1虾', '06', ['1', '2', '2', '13'], ['06', '06', '06']),
+            new Recipe('卷寿司 1饭+1海苔+1黄瓜', '07', ['1', '2', '6'], ['07', '07', '07']),
+
+            new Recipe('anago寿司 1饭+1海苔+2鱼', '08', ['1', '2', '12'], ['08', '08', '08']),
+            new Recipe('章鱼寿司 2饭+2海苔+2章鱼', '09', ['1', '1', '2', '2', '9', '9'], ['09', '09', '09']),
+            new Recipe('扁口鱼寿司 2饭+1海苔+1扁口鱼', '10', ['1', '1', '2', '7'], ['10', '10', '10']),
+            new Recipe('hamachi 鰤鱼寿司 2饭+1海苔+1鰤鱼', '11', ['1', '1', '2', '10'], ['11', '11', '11']),
+            new Recipe('tai鲷鱼寿司 3饭+1海苔+2鲷鱼', '12', ['1', '1', '1', '2', '11'], ['12', '12', '12']),
+            new Recipe('鯖鱼寿司 1饭+2海苔+2鯖鱼', '13', ['1', '2', '2', '8'], ['13', '13', '13']),
         ]
 
         sushiData.forEach((v, i) => {
@@ -44,7 +56,7 @@ export default class SushiMenu extends cc.Component {
         return recipe || new Recipe('shit', '', [], ['none', 'shit', 'none']);
     }
 
-    start () {
+    start() {
 
     }
 
